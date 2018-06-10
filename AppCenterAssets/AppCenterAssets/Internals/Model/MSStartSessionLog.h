b8e503698025111069c4ef885fcd5b39cd24ed8e
Patrick Nikoletich <pnikoletich@gmail.com>
2018-06-09 19:33:39 -0700

    Drop old models

--- AppCenterAssets/AppCenterAssets/Internals/Model/MSStartSessionLog.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#import "AppCenter+Internal.h"
-#import "MSNoAutoAssignSessionIdLog.h"
-
-@interface MSStartSessionLog : MSAbstractLog <MSNoAutoAssignSessionIdLog>
-
-@end
