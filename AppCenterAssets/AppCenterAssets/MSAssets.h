a83b7ca13531f270c918f7675dc1f093f7c996dd
max-mironov <v-maximi@microsoft.com>
2018-06-25 11:34:18 +0300

    fixed order of folders

--- AppCenterAssets/AppCenterAssets/MSAssets.h
+++ AppCenterAssets/AppCenterAssets/MSAssets.h
@@ -22,7 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (MSAssetsAPI *)initAPIWithBuilder:(MSAssetsBuilder *)builder;
 
-+ (MSAssetsAPI *)makeAPIWithBuilder:(void (^)(MSAssetsBuilder *))builder;;
++ (MSAssetsAPI *)makeAPIWithBuilder:(void (^)(MSAssetsBuilder *))builder;
 
 @end
 
