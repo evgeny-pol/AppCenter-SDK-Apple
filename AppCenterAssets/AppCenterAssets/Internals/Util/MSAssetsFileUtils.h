0795a421bb2a77c4e533e0221f4ed3e2419316fc
alex-oreshko <alexander.oreshko@akvelon.com>
2018-07-04 00:03:58 +0300

    MSAssetsFileUtils copyDirectoryContents

--- AppCenterAssets/AppCenterAssets/Internals/Util/MSAssetsFileUtils.h
+++ AppCenterAssets/AppCenterAssets/Internals/Util/MSAssetsFileUtils.h
@@ -4,4 +4,13 @@
 
 + (BOOL)unzipFileAtPath:(NSString *)path toDestination:(NSString *)destination;
 
+
+/**
+ * Copies the contents of one directory to another. Copies all the contents recursively.
+ *
+ * @param sourceDir path to the directory to copy files from.
+ * @param destDir   path to the directory to copy files to.
+ */
++ (BOOL)copyDirectoryContents:(NSString *)sourceDir toDestination:(NSString *)destDir;
+
 @end
