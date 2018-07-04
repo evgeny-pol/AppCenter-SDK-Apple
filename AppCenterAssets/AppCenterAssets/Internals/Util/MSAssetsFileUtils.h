6ee7c592ea186e4c7322e9772b8d8334f50b523f
alex-oreshko <alexander.oreshko@akvelon.com>
2018-07-04 11:59:03 +0300

    moveFile

--- AppCenterAssets/AppCenterAssets/Internals/Util/MSAssetsFileUtils.h
+++ AppCenterAssets/AppCenterAssets/Internals/Util/MSAssetsFileUtils.h
@@ -2,6 +2,22 @@
 
 @interface MSAssetsFileUtils : NSObject
 
+/**
+ * Appends file path with one more component.
+ *
+ * @param path      base path.
+ * @param component path component to be appended to the base path.
+ * @return new path.
+ */
++ (NSString *)appendComponent:(NSString *)component toPath:(NSString *)path;
+
+/**
+ * Gets files from archive.
+ *
+ * @param path path to zip-archive.
+ * @param destination path for the unzipped files to be saved.
+ * @return A flag indicating success or fail.
+ */
 + (BOOL)unzipFileAtPath:(NSString *)path toDestination:(NSString *)destination;
 
 
@@ -10,7 +26,19 @@
  *
  * @param sourceDir path to the directory to copy files from.
  * @param destDir   path to the directory to copy files to.
+ * @return A flag indicating success or fail.
  */
 + (BOOL)copyDirectoryContents:(NSString *)sourceDir toDestination:(NSString *)destDir;
+
+
+/**
+ * Moves file from one folder to another.
+ *
+ * @param fileToMove  path to the file to be moved.
+ * @param newFolder   path to be moved to.
+ * @param newFileName new name for the file to be moved.
+ * @return YES if success, NO if failed.
+ */
++ (BOOL)moveFile:(NSString *)fileToMove toFolder:(NSString *)newFolder withNewName:(NSString*)newFileName;
 
 @end
--- AppCenterAssets/AppCenterAssets/Internals/Util/MSAssetsFileUtils.m
+++ AppCenterAssets/AppCenterAssets/Internals/Util/MSAssetsFileUtils.m
@@ -5,29 +5,26 @@
 
 @implementation MSAssetsFileUtils
 
-/**
- * Gets files from archive.
- *
- * @param path path to zip-archive.
- * @param destination path for the unzipped files to be saved.
- * @return YES if success, NO if failed.
-*/
-+ (BOOL)unzipFileAtPath:(NSString *)path toDestination:(NSString *)destination {
-    return [SSZipArchive unzipFileAtPath:path
-                    toDestination:destination];
+
++ (NSString *)appendComponent:(NSString *)component toPath:(NSString *)path
+{
+    NSString *result = [path copy];
+    if (![result hasSuffix:@"/"])
+        result = [result stringByAppendingString:@"/"];
+    result = [result stringByAppendingString:component];
+    return result;
 }
 
-/**
- * Copies the contents of one directory to another. Copies all the contents recursively.
- *
- * @param sourceDir path to the directory to copy files from.
- * @param destDir   path to the directory to copy files to.
- * @return YES if success, NO otherwise.
- */
+
++ (BOOL)unzipFileAtPath:(NSString *)path toDestination:(NSString *)destination {
+    return [SSZipArchive unzipFileAtPath:path
+                           toDestination:destination];
+}
+
+
 + (BOOL)copyDirectoryContents:(NSString *)sourceDir toDestination:(NSString *)destDir {
     BOOL isDir;
     NSError *error = nil;
-
     NSFileManager *fileManager = [NSFileManager defaultManager];
     if (!([fileManager fileExistsAtPath:destDir isDirectory:&isDir] && isDir)) {
         if (![fileManager createDirectoryAtPath:destDir withIntermediateDirectories:NO attributes:nil error:&error]) {
@@ -52,5 +49,24 @@
 
     return YES;
 }
+
++ (BOOL)moveFile:(NSString *)fileToMove toFolder:(NSString *)newFolder withNewName:(NSString*)newFileName {
+    BOOL isDir;
+    NSError *error = nil;
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (!([fileManager fileExistsAtPath:newFolder isDirectory:&isDir] && isDir)) {
+        if (![fileManager createDirectoryAtPath:newFolder withIntermediateDirectories:NO attributes:nil error:&error]) {
+            MSLogInfo([MSAssets logTag], @"Can't move file from, unable to create directory: %@", newFolder);
+            return NO;
+        }
+    }
+    NSString *newFilePath = [self appendComponent:newFolder toPath:newFileName];
+    if (![fileManager moveItemAtPath:fileToMove toPath:newFilePath error:&error]) {
+        MSLogInfo([MSAssets logTag], @"Can't move file from %@ to %@", fileToMove, newFilePath);
+        return NO;
+    }
+    return YES;
+}
+
 
 @end
