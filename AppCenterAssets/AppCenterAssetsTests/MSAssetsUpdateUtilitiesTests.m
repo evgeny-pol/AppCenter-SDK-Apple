34c81b9bbe79ad0611e35de2ab4be43849ceca79
alex-oreshko <alexander.oreshko@akvelon.com>
2018-07-25 13:59:20 +0300

    JWT tests

--- AppCenterAssets/AppCenterAssetsTests/MSAssetsUpdateUtilitiesTests.m
+++ AppCenterAssets/AppCenterAssetsTests/MSAssetsUpdateUtilitiesTests.m
@@ -12,8 +12,9 @@ static NSString *const kSampleFileText = @"SampleFileText";
 static NSString *const kSampleSubfolder = @"sampleSubfolder";
 static NSString *const kSampleFileInSubfolder = @"SampleFileInSubfolder";
 static NSString *const kSampleFileInSubfolderText = @"SampleFileInSubfolderText";
-//NSString *const BundleJWTFile = @".codepushrelease";
-
+static NSString *const kFakeSignature = @"fakeSignature";
+static NSString *const kNoFolder = @"noSuchFolder";
+static NSString *const kFakePublicKey = @"-----BEGIN PUBLIC KEY-----\n[PUBLIC_KEY_DATA]\n-----END PUBLIC KEY-----\n";
 
 @interface MSAssetsUpdateUtilitiesTests : XCTestCase
 
@@ -31,6 +32,7 @@ static NSString *const kSampleFileInSubfolderText = @"SampleFileInSubfolderText"
     self.sut = [[MSAssetsUpdateUtilities alloc] initWithSettingManager:_mockSettingManager];
 
     [self createFile:kSampleFile inPath:kSampleFolder withText:kSampleFileText];
+    [self createFile:BundleJWTFile inPath:kSampleFolder withText:kFakeSignature];
     [self createFile:kSampleFileInSubfolder inPath:[kSampleFolder stringByAppendingPathComponent:kSampleSubfolder] withText:kSampleFileInSubfolderText];
 }
 
@@ -58,11 +60,10 @@ static NSString *const kSampleFileInSubfolderText = @"SampleFileInSubfolderText"
 - (void)testIsHashIgnoredFor {
     NSString *__MACOSX = @"__MACOSX/";
     NSString *DS_STORE = @".DS_Store";
-    NSString *ASSETS_METADATA = @".codepushrelease";
     NSString *fileThatIsNotIgnored = @"file";
     XCTAssertTrue([self.sut isHashIgnoredFor:__MACOSX]);
     XCTAssertTrue([self.sut isHashIgnoredFor:DS_STORE]);
-    XCTAssertTrue([self.sut isHashIgnoredFor:ASSETS_METADATA]);
+    XCTAssertTrue([self.sut isHashIgnoredFor:BundleJWTFile]);
     XCTAssertFalse([self.sut isHashIgnoredFor:fileThatIsNotIgnored]);
 }
 
@@ -75,7 +76,7 @@ static NSString *const kSampleFileInSubfolderText = @"SampleFileInSubfolderText"
 }
 
 - (void)testFindEntryPointInFolderFailsNoFolder {
-    NSString *entryPoint = [self.sut findEntryPointInFolder:@"noSuchFolder" expectedFileName:kSampleFile];
+    NSString *entryPoint = [self.sut findEntryPointInFolder:kNoFolder expectedFileName:kSampleFile];
     XCTAssertNil(entryPoint);
 }
 
@@ -96,7 +97,7 @@ static NSString *const kSampleFileInSubfolderText = @"SampleFileInSubfolderText"
 
 - (void)testAddContentsOfFolderToManifestFailsNoFolder {
     NSError *error = nil;
-    NSString *folderPath = @"noSuchFolder";
+    NSString *folderPath = kNoFolder;
     NSMutableArray<NSString *> *manifest = [NSMutableArray<NSString* > array];
     BOOL result = [self.sut addContentsOfFolderToManifest:manifest folderPath:folderPath pathPrefix:@"prefix" error:&error];
     XCTAssertFalse(result);
@@ -115,13 +116,62 @@ static NSString *const kSampleFileInSubfolderText = @"SampleFileInSubfolderText"
 }
 
 - (void)testGetSignatureFilePathFailsNoFolder {
-    NSString *signatureFilePath = [self.sut getSignatureFilePath:@"noSuchFolder"];
+    NSString *signatureFilePath = [self.sut getSignatureFilePath:kNoFolder];
     XCTAssertNil(signatureFilePath);
 }
 
-- (void)testGetSignatureFilePathFailsNoSignature {
+- (void)testGetSignatureFilePathSuccess {
     NSString *signatureFilePath = [self.sut getSignatureFilePath:kSampleFolder];
+    XCTAssertNotNil(signatureFilePath);
+    XCTAssertEqualObjects(signatureFilePath, [kSampleFolder stringByAppendingPathComponent:BundleJWTFile]);
+}
+
+- (void)testGetSignatureFilePathFalsNoSignatureFile {
+    NSString *signatureFilePath = [self.sut getSignatureFilePath:[kSampleFolder stringByAppendingPathComponent:kSampleSubfolder]];
     XCTAssertNil(signatureFilePath);
+}
+
+- (void)testSignatureFor {
+    NSError *error = nil;
+    NSString *result = [self.sut getSignatureFor:kSampleFolder error:&error];
+    XCTAssertNotNil(result);
+    XCTAssertEqualObjects(result, kFakeSignature);
+}
+
+- (void)testSignatureForFailsNoFolder {
+    NSError *error = nil;
+    NSString *result = [self.sut getSignatureFor:kNoFolder error:&error];
+    XCTAssertNotNil(error);
+    NSError *expectedError = [MSAssetsErrorUtils getNoSignatureError:[@"noSuchFolder" stringByAppendingPathComponent:BundleJWTFile]];
+    XCTAssertEqualObjects(error, expectedError);
+    XCTAssertNil(result);
+}
+
+- (void)testSignatureForFailsNoFile {
+    NSError *error = nil;
+    NSString *result = [self.sut getSignatureFor:[kSampleFolder stringByAppendingPathComponent:kSampleSubfolder] error:&error];
+    XCTAssertNotNil(error);
+    XCTAssertNil(result);
+}
+
+- (void)testGetKeyValueFromPublicKeyString {
+    NSString *expectedResult = @"[PUBLIC_KEY_DATA]";
+    NSString *key = [self.sut getKeyValueFromPublicKeyString:kFakePublicKey];
+    XCTAssertEqualObjects(key, expectedResult);
+}
+
+- (void)testVerifyUpdateSignatureForFailsBadSignature {
+    NSError *error = nil;
+    BOOL result = [self.sut verifyUpdateSignatureFor:kSampleFolder expectedHash:@"fakeHash" withPublicKey:kFakePublicKey error:&error];
+    XCTAssertFalse(result);
+    XCTAssertNotNil(error);
+}
+
+- (void)testVerifyUpdateSignatureForFailsNoFolder {
+    NSError *error = nil;
+    BOOL result = [self.sut verifyUpdateSignatureFor:kNoFolder expectedHash:@"fakeHash" withPublicKey:kFakePublicKey error:&error];
+    XCTAssertFalse(result);
+    XCTAssertNotNil(error);
 }
 
 @end
